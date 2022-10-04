97f7b600cfffbe41264d6760350a574be2f349ca
xiaokang <648638730@qq.com>
2022-10-04 22:36:11 +0800

    修改

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
+	"github.com/go-kratos/kratos/v2/middleware/ratelimit"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
@@ -26,6 +27,7 @@ func NewGRPCServer(c *conf.Server, cs *service.CarService, logger log.Logger) *g
 		grpc.Middleware(
 			middleware.Chain(
 				recovery.Recovery(),
+				ratelimit.Server(),
 				tracing.Server(),
 				metrics.Server(
 					metrics.WithSeconds(contrib.NewHistogram(requestHistogram)),
